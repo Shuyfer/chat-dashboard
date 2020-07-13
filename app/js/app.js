3d4be62f48d1de4dc6d70c356394846c31d38b9b
Shuyfer <sveya1@gmail.com>
2020-07-13 10:45:45 +0300

    Load project (only layout)

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // $('body').hide()
+    $('.add-file').hide();
+
+    $(function(){
+        $('#add-files-btn').hover(function(){
+                $('#add-file').show();
+                $('#add-video').show(200);
+                $('#add-image').show(400);
+            },
+        function(){
+            if($('#add-file').is(":hover"))
+                $('.add-file').hide();
+        });
+    });
+});
